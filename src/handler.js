85c5b464862fe5965b5e520cf819fc18a9dcfacc
Maulana Malik <maulana.malik53@gmail.com>
2022-12-23 00:51:56 +0700

    first commit, add  'addBookHandler and route'

---
+++ src/handler.js
@@ -0,0 +1,51 @@
+const { nanoid } = require("nanoid");
+const books = require("./books");
+
+const addBookHandler = (request, h) => {
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+    const id = nanoid(16);
+    const insertedAt = new Date().toISOString;
+    const updatedAt = insertedAt;
+    const finished = pageCount === readPage;
+    const newBooks = {
+        name, year, author, summary, publisher, pageCount, readPage, reading, id, insertedAt, updatedAt, finished,
+    };
+    books.push(newBooks);
+    const isSuccess = books.filter((books) => books.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'buku berhassil di tambahkan',
+            data: {
+                "bookId": id,
+            }
+        });
+        response.code(201);
+        return response;
+    }
+    if (name === undefined) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku, Mohon di isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }if (readPage > pageCount) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    }
+    const response = h.response({
+        status: 'error',
+        message: 'Buku gagal ditambahkan'
+    });
+    response.code(500);
+    return response;
+}
+
+
+module.exports = { addBookHandler };
