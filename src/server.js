2272bf245a7d96b910e24778fbe3a60d62dc58bc
Maulana Malik <maulana.malik53@gmail.com>
2022-12-23 16:58:20 +0700

    addbookhandler

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 const Hapi = require('@hapi/hapi');
-
+const routes = require('./routes');
+ 
+ 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-    });
-    server.route(routes);
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
-}; 
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+    cors: {
+      origin: ['*'],
+    },
+  },
+  });
+  server.route(routes);
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-})
+ 
+ 
+init();
